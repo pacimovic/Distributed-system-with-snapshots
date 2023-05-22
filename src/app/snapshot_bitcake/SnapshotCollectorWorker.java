5f109cd2de7b96600686080d19bc397af060110f
petar <petaracimovic990@gmail.com>
2023-05-22 10:13:20 +0200

    Commit project skeleton

--- src/app/snapshot_bitcake/SnapshotCollectorWorker.java
+++ src/app/snapshot_bitcake/SnapshotCollectorWorker.java
@@ -56,7 +56,7 @@ public class SnapshotCollectorWorker implements SnapshotCollector {
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
+
 					e.printStackTrace();
 				}
 				
